a69305dc382944d0a3c7b6e2caaaf791d01e858a
Taina SSantos Silva <tainassilva25111996@gmail.com>
2024-10-09 14:00:33 -0300

    Primeiro Commit

---
+++ primeraApi/src/main/java/com/primeiroProjeto/primeraAPi/PrimeraAPiApplication.java
@@ -0,0 +1,13 @@
+package com.primeiroProjeto.primeraAPi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeraAPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeraAPiApplication.class, args);
+	}
+
+}
---
+++ primeraApi/src/test/java/com/primeiroProjeto/primeraAPi/PrimeraAPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primeiroProjeto.primeraAPi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeraAPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
